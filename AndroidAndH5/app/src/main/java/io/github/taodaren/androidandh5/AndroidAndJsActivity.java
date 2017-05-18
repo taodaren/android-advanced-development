fcf5b035a7779ca39220d734d8957878da069dab
taodaren <taodaren1125@gmail.com>
2017-05-18 18:13:06 +0800

    初始化布局并实现登录界面

--- AndroidAndH5/app/src/main/java/io/github/taodaren/androidandh5/AndroidAndJsActivity.java
+++ AndroidAndH5/app/src/main/java/io/github/taodaren/androidandh5/AndroidAndJsActivity.java
@@ -2,9 +2,11 @@ package io.github.taodaren.androidandh5;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class AndroidAndJsActivity extends Activity implements View.OnClickListener {
     private EditText etNumber;
@@ -29,7 +31,17 @@ public class AndroidAndJsActivity extends Activity implements View.OnClickListen
     @Override
     public void onClick(View v) {
         if (v == btnLogin) {
-            
+            login();
         }
+    }
+
+    private void login() {
+        String number = etNumber.getText().toString().trim();// trim()方法用于忽略空格
+        String password = etPassword.getText().toString().trim();
+        if (TextUtils.isEmpty(number) || TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "账号或密码为空", Toast.LENGTH_SHORT).show();
+        } else {
+            //登录
+        } 
     }
 }
